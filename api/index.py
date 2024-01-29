55af8f970fe82a82d6ccb69a10968667fd181c47
CCYellowStar2 <149048350+CCYellowStar2@users.noreply.github.com>
2024-01-29 20:22:52 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ def get_location_info_from_ip2location(api_key, ip_address):
     data = response.json()
     return data
 
-@app.route('/')
+@app.route('/1')
 def redirect_based_on_location():
     user_ip = request.remote_addr  # 获取用户的 IP 地址
     api_key = "2C4BFA3DFA445146D966BB3927CD4D87"  # 替换成你的IP2Location API密钥
@@ -17,8 +17,8 @@ def redirect_based_on_location():
     location_info = get_location_info_from_ip2location(api_key, user_ip)
     
     if location_info["country_code"] == "CN":
-        new_url = 'http://123.ccyacg.xyz' + request.full_path  # 拼接国内用户的新链接
+        new_url = 'http://123.ccyacg.xyz/1' + request.full_path  # 拼接国内用户的新链接
         return redirect(new_url, code=302)  # 302 重定向
     else:
-        new_url = 'http://pan.ccyacg.xyz' + request.full_path  # 拼接国外用户的新链接
+        new_url = 'http://pan.ccyacg.xyz/1' + request.full_path  # 拼接国外用户的新链接
         return redirect(new_url, code=302)  # 302 重定向
